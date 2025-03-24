fdfaf1bd29c9b439658058fecd31eb30180178f8
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-03-24 16:31:39 +0330

    routing, empty components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './Components/Home'
+import Characters from './Components/Characters.JSX'
+import Story from './Components/Story'
 
 const App = () => {
   return (
     <Router>
       <nav>
-        <Link to=''>Home</Link>
-        <Link to=''>Characters</Link>
-        <Link to=''>Ring's Story</Link>
+        <Link to='/'>Home</Link>
+        <Link to='/characters'>Characters</Link>
+        <Link to='/story'>Ring's Story</Link>
       </nav>
 
       <Routes>
-        <Route path='' element={}/>
-        <Route path='' element={}/>
-        <Route path='' element={}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='characters' element={<Characters/>}/>
+        <Route path='story' element={<Story/>}/>
       </Routes>
     </Router>
   )
